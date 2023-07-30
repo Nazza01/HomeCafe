a9c0a351784f53b83b7fc2e6ed7aba40164e60ac
Nazza01 <nate_thomas_95@hotmail.com>
2023-07-30 22:35:19 +0930

    adding side menu

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,14 +1,36 @@
 import axios from "axios";
+import PropTypes from 'prop-types';
 import { useEffect, useState } from "react";
 import { API } from "../constant";
 import { getToken } from "../hooks/useLocalStorage";
+import Box from "@mui/material/Box";
+import Tabs, { tabsClasses } from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from "@mui/material/Typography";
+
+function TabPanel({ children, value, index, ...other }) {
+  return (
+    <div role="tabpanel" hidden={value !== index} {...other}>
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 const MenuList = () => {
+  const [value, setValue] = useState(0);
   const [error, setError] = useState(null);
   const [menuTypes, setMenuTypes] = useState([]);
   
   const config = {
     headers: { Authorization: `Bearer ${getToken()}` }
+  };
+  
+  const handleChange = (_value, newValue) => {
+    setValue(newValue);
   };
   
   useEffect(() => {
@@ -23,13 +45,35 @@ const MenuList = () => {
   }
 
   return (
-    <div>
-      <ul>
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: 'background.paper',
+        display: 'flex',
+        height: 224
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="tab section describing all different types of elements"
+        sx={{
+          borderRight: 1,
+          borderColor: 'divider'
+        }}
+      >
         {menuTypes.map(({ id, attributes }) => (
-          <li key={id}>{attributes.name}</li>
+          <Tab key={id} label={attributes.name} />
         ))}
-      </ul>
-    </div>
+      </Tabs>
+      {menuTypes.map(({ id, attributes }) => (
+        <TabPanel key={id} value={attributes} index={id}>
+          {attributes.slug}
+        </TabPanel>
+      ))}
+    </Box>
   )
 };
 
